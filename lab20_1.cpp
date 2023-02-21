12b3cb17d22dbdd688e61dc56eefb84de6fd4a06
GOLF2546 <Pattarapolnanakorn@gmail.com>
2023-02-21 16:53:12 +0700

    OKOK

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,14 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1 ,Rect R2){
+		double a = max(R1.x,R2.x);
+		double b = min(R1.x+R1.w,R2.x+R2.w);
+		double c = min(R1.y,R2.y);
+		double d = max(R1.y-R1.h,R2.y-R2.h);
+	if(b-a > 0){
+		return (b-a)*(c-d);
+	}else{
+	    return 0;
+	    }
 }
